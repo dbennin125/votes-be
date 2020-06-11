c86427fd21cb4957fbf35110a1b549e8be53f7b8
Daniel Bennington <dbennington125@gmail.com>
2020-06-10 18:29:05 -0700

    ready to test vote

---
+++ __tests__/votes.test.js
@@ -0,0 +1,62 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const Poll = require('../lib/models/Poll');
+const Organization = require('../lib/models/Organization');
+const User = require('../lib/models/User');
+const Vote = require('../lib/models/Vote');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('vote routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let organization;
+  beforeEach(async() => {
+    organization = await Organization.create({
+      title: 'Brunch Club',
+      description: 'A club for brunch',
+      imageUrl: 'somestring',
+    });
+  });
+    
+  let poll;
+  beforeEach(async() => {
+    poll = await Poll.create({
+      organization: organization._id,
+      title: 'Casual Friday',
+      description: 'Jean and T-shirts',
+      list: 'option1'
+    });
+  });
+      
+  let user;
+  beforeEach(async() => {
+    user = await User.create({
+      name: 'Bob',
+      phone: '15031112222',
+      email: 'not@realmail.com',
+      communicationMedium: ['phone'],
+      imageUrl: 'somestring'
+    });
+  });
+  
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+    
+  it('creates a vote via POST', async() => {
+        
+  });
+});
